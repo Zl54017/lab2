906c32556adae8ac9263f1f194237dcadb6d21d0
Zvonko Lelas <zl54017@fer.hr>
2024-11-10 22:26:49 +0100

    final2

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ import routes from './routes';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', routes);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
