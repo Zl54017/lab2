7c1fbdc59c22895fac816e2b2c06a3b2fb23f3ce
Zvonko Lelas <zl54017@fer.hr>
2024-11-10 16:02:00 +0100

    dsd

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ import routes from './routes';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, '..', 'views')));
 
 app.use('/', routes);
 
-const PORT = "https://lab2-d767.onrender.com/";
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
